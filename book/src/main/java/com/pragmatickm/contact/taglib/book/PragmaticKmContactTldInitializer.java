8e5b43f0a045922916c37eb7584090b0c357089d
AO Industries, Inc <support@aoindustries.com>
2020-06-03 17:16:17 -0500

    Now using String varargs instead of generic Map

--- book/src/main/java/com/pragmatickm/contact/taglib/book/PragmaticKmContactTldInitializer.java
+++ book/src/main/java/com/pragmatickm/contact/taglib/book/PragmaticKmContactTldInitializer.java
@@ -23,7 +23,6 @@
 package com.pragmatickm.contact.taglib.book;
 
 import com.semanticcms.tagreference.TagReferenceInitializer;
-import java.util.Collections;
 
 public class PragmaticKmContactTldInitializer extends TagReferenceInitializer {
 
@@ -37,7 +36,7 @@ public class PragmaticKmContactTldInitializer extends TagReferenceInitializer {
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
 			Maven.properties.getProperty("documented.javadoc.link.javaee"),
 			// Self
-			Collections.singletonMap("com.pragmatickm.contact.taglib", Maven.properties.getProperty("project.url") + "apidocs/")
+			"com.pragmatickm.contact.taglib", Maven.properties.getProperty("project.url") + "apidocs/"
 		);
 	}
 }
