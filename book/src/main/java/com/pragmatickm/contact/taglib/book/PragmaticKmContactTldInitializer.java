d288c985eb30eeb8871d08bf405ff5360899d378
AO Industries, Inc <support@aoindustries.com>
2020-07-25 15:00:35 -0500

    Removed unexpected "Book" in tag reference page titles

--- book/src/main/java/com/pragmatickm/contact/taglib/book/PragmaticKmContactTldInitializer.java
+++ book/src/main/java/com/pragmatickm/contact/taglib/book/PragmaticKmContactTldInitializer.java
@@ -33,7 +33,7 @@ public class PragmaticKmContactTldInitializer extends TagReferenceInitializer {
 
 	public PragmaticKmContactTldInitializer() throws ValidationException {
 		super(
-			Maven.properties.getProperty("project.name") + " Reference",
+			Maven.properties.getProperty("documented.name") + " Reference",
 			"Taglib Reference",
 			new ResourceRef(
 				new BookRef(
