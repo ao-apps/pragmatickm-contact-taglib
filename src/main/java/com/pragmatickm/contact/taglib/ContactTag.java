b13b4c632ffd70ab1e8e9c2d8e366eb345577dd8
AO Industries, Inc <support@aoindustries.com>
2021-02-14 08:19:23 -0600

    Released ao-encoding-3.3.0

--- src/main/java/com/pragmatickm/contact/taglib/ContactTag.java
+++ src/main/java/com/pragmatickm/contact/taglib/ContactTag.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-contact-taglib - Contacts nested within SemanticCMS pages and elements in a JSP environment.
- * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2020  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2020, 2021  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,12 +22,12 @@
  */
 package com.pragmatickm.contact.taglib;
 
-import com.aoindustries.encoding.Coercion;
 import com.aoindustries.encoding.Doctype;
 import com.aoindustries.encoding.Serialization;
 import com.aoindustries.encoding.servlet.DoctypeEE;
 import com.aoindustries.encoding.servlet.SerializationEE;
 import com.aoindustries.html.Html;
+import com.aoindustries.lang.Coercion;
 import com.aoindustries.lang.Strings;
 import com.aoindustries.net.Email;
 import static com.aoindustries.taglib.AttributeUtils.resolveValue;
