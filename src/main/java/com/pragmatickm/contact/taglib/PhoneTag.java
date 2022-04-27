5c616a4ac83e6309c6be3fa1e899e4d3e22d9d0a
AO Industries, Inc <support@aoindustries.com>
2022-04-27 00:14:54 -0500

    Performed automatic clean-up with org.openrewrite.java.cleanup.Cleanup

--- src/main/java/com/pragmatickm/contact/taglib/PhoneTag.java
+++ src/main/java/com/pragmatickm/contact/taglib/PhoneTag.java
@@ -144,8 +144,8 @@ public class PhoneTag extends SimpleTagSupport implements ElementWriter {
   @Override
   public void writeTo(Writer out, ElementContext context) throws IOException {
     new Document(serialization, doctype, characterEncoding, out)
-        .setAutonli(false) // Do not add extra newlines to JSP
-        .setIndent(false)  // Do not add extra indentation to JSP
+        .setAutonli(false)// Do not add extra newlines to JSP
+        .setIndent(false)// Do not add extra indentation to JSP
         .span().clazz(newPhoneNumber.getType().getCssClass()).__(newPhoneNumber.getNumber());
   }
 }
